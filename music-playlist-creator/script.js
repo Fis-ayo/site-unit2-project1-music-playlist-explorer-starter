7d0587b490f3388430fc89200bcabbb5ae4322c9
Mercy Fisayo Akinyemi <mercyakinyemi@meta.com>
2025-06-04 15:36:13 -0700

    Updated script.jss for modal window

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,54 +1,65 @@
 // Function to display each of the playlist cards
+const details = document.querySelector(".modal-content");
 
 function renderPlaylists() {
     fetch('data/data.json')
-    .then(response => response.json())
-    .then(playlists => {
-        const container = document.querySelector('.playlist-cards');
-        container.innerHTML = '';
+        .then(response => response.json())
+        .then(playlists => {
+            const container = document.querySelector('.playlist-cards');
+            container.innerHTML = '';
 
-        playlists.forEach((playlist, index) => {
-            const card = document.createElement('div');
-            card.className = 'playlist-card';
+            playlists.forEach((playlist, index) => {
+                const card = document.createElement('div');
+                card.className = 'playlist-card';
 
-            card.innerHTML = `
-                <img src="${playlist.playlist_cover}" alt="${playlist.playlist_name} Cover" class="cover-img" width = 100px/>
-                <h3 id="playlist_name">${playlist.playlist_name}</h3>
-                <p i>Created by ${playlist.playlist_author}</p>
+                card.innerHTML = `
+                <img src="${playlist.playlist_cover}" alt="${playlist.playlist_name} Cover" class="cover-img"/>
+                <h3>${playlist.playlist_name}</h3>
+                <p>Created by ${playlist.playlist_author}</p>
                 <button class="like-btn" data-index="${index}">
                 ❤️ <span class="like-cnt">${playlist.playlist_likes}</span>
                 </button>
-            `;
+                `;
 
-            container.appendChild(card);
-        });
+                container.appendChild(card);
 
-        document.querySelectorAll('.like-btn').forEach(button => {
-            let liked = false;
-
-            button.addEventListener("click", () => {
-                const countSpan = button.querySelector('.like-cnt');
-                let likes = parseInt(countSpan.textContent);
-
-                liked = !liked;
-                likes += liked ? 1: -1;
-
-                countSpan.textContent = likes;
-                button.classList.toggle('liked');
+                const modal = document.querySelector(".modal-overlay");
+                card.addEventListener('click', () => {
+                modal.style.display = 'flex';
+                details.innerHTML = `
+                    <span class="close">&times;</span>
+                    <img src="${playlist.playlist_cover}" alt="${playlist.playlist_name} Cover" class="detail-img"/>
+                    <h3>${playlist.playlist_name}</h3>
+                    <p>${playlist.playlist_author}</p>
+                    <button class="shuffle-btn">shuffle</button>
+                `;
+                
             });
-        });
-    })
-    .catch(error => console.error('Error fetching playlist:', error));
-}
+            });
+
+            
+
+
+            document.querySelectorAll('.like-btn').forEach(button => {
+                let liked = false;
+
+                button.addEventListener("click", () => {
+                    const countSpan = button.querySelector('.like-cnt');
+                    let likes = parseInt(countSpan.textContent);
+
+                    liked = !liked;
+                    likes += liked ? 1 : -1;
+
+                    countSpan.textContent = likes;
+                    button.classList.toggle('liked');
+                });
+            });
+
+
+        })
+        .catch(error => console.error('Error fetching playlist:', error));
+};
 
 renderPlaylists();
 
-// Function to view playlist details
-const modal = document.querySelector(".modal-overlay");
-const playlist_imgs = document.querySelectorAll(".cover-img");
 
-playlist_imgs.forEach(image => {
-    image.addEventListener("click", () => {
-        modal.style.display = 'flex';
-    });
-});

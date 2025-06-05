e9ef59446cb672caff03f244c82b70d4caf75131
Mercy Fisayo Akinyemi <mercyakinyemi@meta.com>
2025-06-05 15:24:42 -0700

    Updated main features for main page

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,6 +1,8 @@
 // Function to display each of the playlist cards
 const details = document.querySelector(".modal-content");
 
+const heartIcon = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path fill="#e222af" d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/></svg>'
+const heartIcon_plain = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path fill="#f83091" d="M225.8 468.2l-2.5-2.3L48.1 303.2C17.4 274.7 0 234.7 0 192.8l0-3.3c0-70.4 50-130.8 119.2-144C158.6 37.9 198.9 47 231 69.6c9 6.4 17.4 13.8 25 22.3c4.2-4.8 8.7-9.2 13.5-13.3c3.7-3.2 7.5-6.2 11.5-9c0 0 0 0 0 0C313.1 47 353.4 37.9 392.8 45.4C462 58.6 512 119.1 512 189.5l0 3.3c0 41.9-17.4 81.9-48.1 110.4L288.7 465.9l-2.5 2.3c-8.2 7.6-19 11.9-30.2 11.9s-22-4.2-30.2-11.9zM239.1 145c-.4-.3-.7-.7-1-1.1l-17.8-20-.1-.1s0 0 0 0c-23.1-25.9-58-37.7-92-31.2C81.6 101.5 48 142.1 48 189.5l0 3.3c0 28.5 11.9 55.8 32.8 75.2L256 430.7 431.2 268c20.9-19.4 32.8-46.7 32.8-75.2l0-3.3c0-47.3-33.6-88-80.1-96.9c-34-6.5-69 5.4-92 31.2c0 0 0 0-.1 .1s0 0-.1 .1l-17.8 20c-.3 .4-.7 .7-1 1.1c-4.5 4.5-10.6 7-16.9 7s-12.4-2.5-16.9-7z"/></svg>'
 
 function renderPlaylists() {
     fetch('data/data.json')
@@ -9,7 +11,7 @@ function renderPlaylists() {
             const container = document.querySelector('.playlist-cards');
             container.innerHTML = '';
 
-            if(playlists.length === 0){
+            if (playlists.length === 0) {
                 container.innerHTML = `<p class="emptyPlaylist"> No Playlist Added </p>`;
                 return;
             }
@@ -22,15 +24,16 @@ function renderPlaylists() {
                 <img src="${playlist.playlist_cover}" alt="${playlist.playlist_name} Cover" class="cover-img"/>
                 <h3>${playlist.playlist_name}</h3>
                 <p>Created by ${playlist.playlist_author}</p>
-                <button class="like-btn" data-index="${index}">
-                ❤️ <span class="like-cnt">${playlist.playlist_likes}</span>
-                </button>
+                <div class="like-btn" data-index="${index}">
+                <div class="Icon">${heartIcon_plain} </div> <span class="like-cnt">${playlist.playlist_likes}</span>
+                </div>
                 `;
 
                 container.appendChild(card);
 
                 const modal = document.querySelector(".modal-overlay");
-                card.addEventListener('click', () => {
+                const art = document.querySelector(".cover-img");
+                art.addEventListener('click', () => {
                     modal.style.display = 'flex';
 
                     details.innerHTML = `
@@ -73,7 +76,7 @@ function renderPlaylists() {
                     });
 
                     window.addEventListener('click', (event) => {
-                        if(event.target === modal) {
+                        if (event.target === modal) {
                             modal.style.display = 'none';
                         }
                     });
@@ -83,8 +86,8 @@ function renderPlaylists() {
                         const songContainer = document.querySelector('.songs');
                         const cards = Array.from(songContainer.children);
 
-                        for(let i=cards.length-1; i>0; i--){
-                            const j = Math.floor(Math.random() * (i+1));
+                        for (let i = cards.length - 1; i > 0; i--) {
+                            const j = Math.floor(Math.random() * (i + 1));
                             [cards[i], cards[j]] = [cards[j], cards[i]];
                         }
 
@@ -95,11 +98,11 @@ function renderPlaylists() {
                 });
             });
 
-
             document.querySelectorAll('.like-btn').forEach(button => {
+                let icon = button.querySelector(".Icon");
                 let liked = false;
 
-                button.addEventListener("click", () => {
+                icon.addEventListener("click", () => {
                     const countSpan = button.querySelector('.like-cnt');
                     let likes = parseInt(countSpan.textContent);
 
@@ -108,7 +111,14 @@ function renderPlaylists() {
 
                     countSpan.textContent = likes;
                     button.classList.toggle('liked');
+                    console.log(liked)
+                    if (liked == true) {
+                        icon.innerHTML = `${heartIcon}`;
+                    } else {
+                        icon.innerHTML = `${heartIcon_plain}`;
+                    }
                 });
+
             });
 
 
@@ -116,6 +126,23 @@ function renderPlaylists() {
         .catch(error => console.error('Error fetching playlist:', error));
 };
 
+const addPlaylist = document.getElementById('add-playlist-btn');
+const formOverlay = document.getElementById('form-overlay');
+const closePlaylist = document.querySelector(".close");
+
+closePlaylist.addEventListener('click', () => {
+    formOverlay.style.display = 'none';
+});
+
+// window.addEventListener('click', (event) => {
+//     if (event.target === modal) {
+//         modal.style.display = 'none';
+//     }
+// });
+
+addPlaylist.addEventListener("click", () => {
+    formOverlay.style.display = 'flex';
+})
 renderPlaylists();
 
 

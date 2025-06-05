fca27ef2b68c5af97519983e89f7f8aa699913f3
Mercy Fisayo Akinyemi <mercyakinyemi@meta.com>
2025-06-05 01:21:19 -0700

    Updated all files

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,12 +1,18 @@
 // Function to display each of the playlist cards
 const details = document.querySelector(".modal-content");
 
+
 function renderPlaylists() {
     fetch('data/data.json')
         .then(response => response.json())
         .then(playlists => {
             const container = document.querySelector('.playlist-cards');
             container.innerHTML = '';
+
+            if(playlists.length === 0){
+                container.innerHTML = `<p class="emptyPlaylist"> No Playlist Added </p>`;
+                return;
+            }
 
             playlists.forEach((playlist, index) => {
                 const card = document.createElement('div');
@@ -25,19 +31,69 @@ function renderPlaylists() {
 
                 const modal = document.querySelector(".modal-overlay");
                 card.addEventListener('click', () => {
-                modal.style.display = 'flex';
-                details.innerHTML = `
-                    <span class="close">&times;</span>
-                    <img src="${playlist.playlist_cover}" alt="${playlist.playlist_name} Cover" class="detail-img"/>
-                    <h3>${playlist.playlist_name}</h3>
-                    <p>${playlist.playlist_author}</p>
-                    <button class="shuffle-btn">shuffle</button>
-                `;
-                
-            });
-            });
+                    modal.style.display = 'flex';
 
-            
+                    details.innerHTML = `
+                    <span class="close"> &times; </span>
+                    <div class="playlist-head">
+                        <img src="${playlist.playlist_cover}" alt="${playlist.playlist_name} Cover" class="detail-img"/>
+                        <div class = "playlist-text">
+                            <h3>${playlist.playlist_name}</h3>
+                            <p>${playlist.playlist_author}</p>
+                        </div>
+                    </div>
+                    <div>
+                    <button class="shuffle-btn">Shuffle</button>
+                    </div>
+
+                    <div class="songs"></div>
+                    `;
+                    const playlistSongs = document.querySelector(".songs");
+                    playlist.songs.forEach((songDetail, index) => {
+
+                        const song = document.createElement('div');
+                        song.className = 'playlist-song';
+                        song.innerHTML = `
+                        <img src="${songDetail.song_cover}" alt="${songDetail.songTitle} Cover" class="song-img"/>
+
+                        <div class = "textDetail">
+                                <h3>${songDetail.songTitle}</h3>
+                                <p>${songDetail.artistName}</p>
+                                <p>${songDetail.albumName}</p>
+                        </div>
+
+                        <p class="duration">${songDetail.songDuration}</p>
+                        `;
+                        playlistSongs.appendChild(song);
+                    });
+
+                    const closeBtn = document.querySelector(".close");
+                    closeBtn.addEventListener('click', () => {
+                        modal.style.display = 'none';
+                    });
+
+                    window.addEventListener('click', (event) => {
+                        if(event.target === modal) {
+                            modal.style.display = 'none';
+                        }
+                    });
+
+                    const shuffle = document.querySelector(".shuffle-btn");
+                    shuffle.addEventListener('click', () => {
+                        const songContainer = document.querySelector('.songs');
+                        const cards = Array.from(songContainer.children);
+
+                        for(let i=cards.length-1; i>0; i--){
+                            const j = Math.floor(Math.random() * (i+1));
+                            [cards[i], cards[j]] = [cards[j], cards[i]];
+                        }
+
+                        songContainer.innerHTML = "";
+                        cards.forEach(card => songContainer.appendChild(card));
+                    });
+
+                });
+            });
 
 
             document.querySelectorAll('.like-btn').forEach(button => {
